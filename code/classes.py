7e2ccce837ea3e1dd6094eabb22449279403532c
Pedroca2005BR <pedrodepaula2005@gmail.com>
2022-08-18 09:30:58 -0300

    Reparando classes

---
+++ code/classes.py
@@ -0,0 +1,11 @@
+import pygame
+
+from config import PlayerY
+
+class Player:
+    def __init__(self):
+        pass
+
+
+def move(self):
+    PlayerX += PlayerX_change
---
+++ config.py
@@ -0,0 +1,5 @@
+X = 700
+Y = 500
+
+PlayerX = X/2 - 32
+PlayerY = Y - 100
